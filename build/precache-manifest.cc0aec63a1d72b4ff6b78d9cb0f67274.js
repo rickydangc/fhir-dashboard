3beff1595a81bf77cc6fd351d10be82d8957521d
Chong Dang <chong.dang@gatech.edu>
2020-10-06 00:23:15 -0400

    deploy changes

---
+++ build/precache-manifest.cc0aec63a1d72b4ff6b78d9cb0f67274.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81e2754c9ee149dafb68da499c4debe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "159332f2a74821b39494",
+    "url": "/static/css/2.279ec1ea.chunk.css"
+  },
+  {
+    "revision": "af60d868f012ef2ebc33",
+    "url": "/static/css/main.42d65ea7.chunk.css"
+  },
+  {
+    "revision": "159332f2a74821b39494",
+    "url": "/static/js/2.0abc533e.chunk.js"
+  },
+  {
+    "revision": "c1adb6ef1725aa882efc8e0b341ed4e8",
+    "url": "/static/js/2.0abc533e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af60d868f012ef2ebc33",
+    "url": "/static/js/main.761e811e.chunk.js"
+  },
+  {
+    "revision": "750ff6717d2fda6071b1",
+    "url": "/static/js/runtime-main.f826a5c3.js"
+  },
+  {
+    "revision": "6fdd1fb1d96b73f997bd949a114919a4",
+    "url": "/static/media/avatar.6fdd1fb1.jpg"
+  },
+  {
+    "revision": "526965d0adc32f9e74862da495b34e46",
+    "url": "/static/media/fhir-logo-long.526965d0.png"
+  },
+  {
+    "revision": "d92a001499434d64c0cff19dd36b4f6d",
+    "url": "/static/media/fhir_logo.d92a0014.png"
+  },
+  {
+    "revision": "9037a8f578ea55c4c003d82edd6bf814",
+    "url": "/static/media/homebg2.9037a8f5.jpg"
+  },
+  {
+    "revision": "5048c7b070a822e355c76b1624b191c9",
+    "url": "/static/media/icon_patient.5048c7b0.png"
+  },
+  {
+    "revision": "a81d38a5cfb622a02213ff7135a8d2a6",
+    "url": "/static/media/icon_search2.a81d38a5.png"
+  },
+  {
+    "revision": "904d5ec3cd318088c895b3a3868bcbcc",
+    "url": "/static/media/icon_stats2.904d5ec3.png"
+  }
+]);
